c060b904a6c904e57f4dd8bc98c9801aa5d9b2c5
Marvin Hofmann <marvin@marv-productions.de>
2016-07-29 18:06:39 +0200

    Updated collector to give more info and shutdown graceful on problems

--- collector.py
+++ collector.py
@@ -1,4 +1,5 @@
 import json
+import numbers
 import os
 
 import re
@@ -7,6 +8,7 @@ from twisted.internet.defer import Deferred, inlineCallbacks
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
+from twisted.python import log
 
 ignore_file = "res/.uploader.ignore"
 
@@ -66,6 +68,9 @@ def stop_reaktor(_):
 
 
 def shutdown(seconds, result=None):
+    if not isinstance(seconds, numbers.Number):
+        log.err(seconds)
+        seconds = 1
     d = Deferred()
     d.addCallback(stop_reaktor)
     reactor.callLater(seconds, d.callback, result)
@@ -84,7 +89,9 @@ def find_files():
     ignored = []
     if os.path.isfile(ignore_file):
         with open(ignore_file) as f:
-            ignored = f.readlines()
+            for line in f:
+                ignored.append(line.strip().replace("res/", ''))
+
     for file in os.listdir("res"):
         if pattern.match(file) is not None and file not in ignored:
             fileout = "res/" + file
@@ -96,12 +103,15 @@ def file_len(fname):
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
-    return i + 1
+    s = i + 1 - 1
+    if s < 1:
+        s = 1
+    return s
 
 
 def complete_file(file):
     with open(ignore_file, 'a') as f:
-        f.write(file + "\n")
+        f.write(file.replace("res/", '') + "\n")
 
 
 @inlineCallbacks
@@ -109,29 +119,40 @@ def gotProtocol(p):
     for file in find_files():
         if os.path.isfile(file):
             linenum = file_len(file)
+            current = 0
             with open(file) as f:
+                print("found file {} with {} pokemon".format(file.replace("res/", ''), linenum))
                 for line in f:
                     if line.startswith("Name"):
                         continue
+                    current += 1
                     p.sendMessage(line)
                     # Give control back to the reactor to actually send the data
-                    update_tick()
+                    update_tick(linenum=linenum, current=current)
                     yield wait(0.1)
             complete_file(file)
+
+    if update_tick.counter == 0:
+        print('')
+        print("You seem to have no uploadable files.")
+        print("Wait a bit for the map to accumulate more pokemon.")
 
     print('')
     p.transport.loseConnection()
     print("Transfered {} pokemon sightings!".format(update_tick.counter))
     print("Transfer done, waiting for data to be send by the os!")
-    yield shutdown(10)
+    yield shutdown(1 + (update_tick.counter / 100))
     print("Thanks for helping out!")
 
 
-def update_tick(dot='#'):
+def update_tick(dot='#', linenum=None, current=None):
     update_tick.counter += 1
     print(dot, end='', flush=True)
     if update_tick.counter % 100 == 0:
         print('')
+        if linenum and current:
+            perc = int((float(current) / float(linenum) * 10000) / 100)
+            print("You have send {}/{} from this file ({}%)!".format(current, linenum, perc))
         print('Already send {} pokemon this session!'.format(update_tick.counter))
 
 
@@ -141,7 +162,6 @@ if __name__ == '__main__':
     point = TCP4ClientEndpoint(reactor, url, port)
     d = point.connect(GreeterFactory())
     d.addCallback(gotProtocol)
-    from twisted.python import log
 
-    d.addErrback(log.err)
+    d.addErrback(shutdown)
     reactor.run()
