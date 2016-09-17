6f7b7c6fc4ef210466f6722b2d9a87ba88c7c3bb
seikur0 <seikur00@gmail.com>
2016-09-17 22:46:18 +0200

    fix for #210 #212

--- main0.py
+++ main0.py
@@ -451,18 +451,20 @@ def login_ptc(account):
 
         except Exception as e:
             if r is not None:
-                answer = json.loads(r.content)
-                if answer.get('error_code',None) == 'users.login.activation_required':
-                    lprint('[{}] Login error for {}, needs email verification.'.format(account['num'],account['user']))
-                    exit()
-                elif answer.get('errors',None)[0].startswith('Your username or password is incorrect.'):
-                    lprint('[{}] Login error for {}, incorrect username/password/account does not exist.'.format(account['num'], account['user']))
-                    exit()
-                elif answer.get('errors',None)[0].startswith('As a security measure, your account has been disabled for 15 minutes'):
-                    lprint('[{}] Login error for {}, incorrect username/password was entered 5 times, login for that account is disabled for 15 minutes.'.format(account['num'], account['user']))
-                    exit()
-                lprint('[{}] Ptc login error in step {}: {}'.format(account['num'], step, e))
-                lprint('[{}] Connection error, http code: {}, content: {}'.format(account['num'], r.status_code, answer))
+                try:
+                    answer = json.loads(r.content)
+                    if answer.get('error_code',None) == 'users.login.activation_required':
+                        lprint('[{}] Login error for {}, needs email verification.'.format(account['num'],account['user']))
+                        exit()
+                    elif answer.get('errors',None)[0].startswith('Your username or password is incorrect.'):
+                        lprint('[{}] Login error for {}, incorrect username/password/account does not exist.'.format(account['num'], account['user']))
+                        exit()
+                    elif answer.get('errors',None)[0].startswith('As a security measure, your account has been disabled for 15 minutes'):
+                        lprint('[{}] Login error for {}, incorrect username/password was entered 5 times, login for that account is disabled for 15 minutes.'.format(account['num'], account['user']))
+                        exit()
+                    lprint('[{}] Connection error, http code: {}, content: {}'.format(account['num'], r.status_code, answer))
+                except ValueError:
+                    lprint('[{}] Ptc login error in step {}: {}'.format(account['num'], step, e))
             else:
                 lprint('[{}] Ptc login error in step {}: {}'.format(account['num'], step, e))
                 lprint('[{}] Error happened before network request.'.format(account['num']))
