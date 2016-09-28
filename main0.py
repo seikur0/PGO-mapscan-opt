ab16000af4ea2c928ded30ee6a9286a05ba7b8d3
seikur0 <seikur00@gmail.com>
2016-09-28 13:04:29 +0200

    reverted notification disable for map in telegram
    at least under Android 6, that leads to no notifications at all (bug in the telegram api)

--- main0.py
+++ main0.py
@@ -1780,7 +1780,7 @@ def main():
                                 for telegram in telegrams:
                                     try:
                                         telebot.sendMessage(chat_id=telegram, text=u'<b>{}</b>\n{}'.format(notification_text,time_text), parse_mode='HTML', disable_web_page_preview=False, disable_notification=False)
-                                        telebot.sendLocation(chat_id=telegram, latitude=wild.latitude, longitude=wild.longitude, disable_notification=True)
+                                        telebot.sendLocation(chat_id=telegram, latitude=wild.latitude, longitude=wild.longitude, disable_notification=False)
                                     except Exception as e:
                                         lprint('[-] Connection Error during Telegram, error: {}'.format(e))
                             if addpokemon.empty() and time.time() < nextdatwrite:
